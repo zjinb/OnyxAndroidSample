82e4316c84dfd66c3d9b3d80ff3e4a95900d4c1c
yangzp <yangzp@onyx-international.com>
2017-04-06 19:57:41 +0800

    Resolve ProgressBar progress display is not normal .

--- app/music/src/main/java/com/onyx/music/MediaPlaybackActivity.java
+++ app/music/src/main/java/com/onyx/music/MediaPlaybackActivity.java
@@ -1173,7 +1173,7 @@ public class MediaPlaybackActivity extends Activity implements MusicUtils.Defs,
                     break;
 
                 case REFRESH:
-                    long next = 8*refreshNow();
+                    long next = refreshNow();
                     queueNextRefresh(next);
                     break;
                     
