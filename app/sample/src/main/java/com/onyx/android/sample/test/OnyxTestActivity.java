4b59a66687e3f2a4b0a8cc36942719b372bcbd4e
anypwx <531884915@qq.com>
2018-04-11 18:37:52 +0800

    style(OnyxAndroidSample): optimize code

--- app/sample/src/main/java/com/onyx/android/sample/test/OnyxTestActivity.java
+++ app/sample/src/main/java/com/onyx/android/sample/test/OnyxTestActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.onyx.android.sample.AppOptimizeActivity;
 import com.onyx.android.sample.R;
 
 import butterknife.ButterKnife;
@@ -51,6 +52,11 @@ public class OnyxTestActivity extends AppCompatActivity {
     @OnClick(R.id.button_rect_update)
     void button_rect_update() {
         startActivity(new Intent(this, RectangleUpdateTest.class));
+    }
+
+    @OnClick(R.id.btn_broadcast_test)
+    public void btn_broadcast_test(){
+        startActivity(new Intent(this,AppOptimizeActivity.class));
     }
 
 }
