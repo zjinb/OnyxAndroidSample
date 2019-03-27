b511c34e7242c5bd5d39fbd57e7bd537b40e7e0b
Joy <joy@onyx-international.com>
2019-03-27 12:26:17 +0800

    sample: add WebViewOptimizeActivity demo

---
+++ app/sample/src/main/java/com/onyx/android/sample/WebViewOptimizeActivity.java
@@ -0,0 +1,33 @@
+package com.onyx.android.sample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
+
+import com.onyx.android.sdk.api.device.epd.EpdController;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class WebViewOptimizeActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    private boolean toggled = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_webview_optimize);
+        ButterKnife.bind(this);
+
+        webView = findViewById(R.id.web_view);
+        webView.loadUrl("http://www.baidu.com");
+    }
+
+    @OnClick(R.id.button_toggle_optimize)
+    public void toggleOptimize() {
+        toggled = !toggled;
+        EpdController.setWebViewContrastOptimize(webView, toggled);
+    }
+}
